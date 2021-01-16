dc261085696ef63e9705478a05bc9d3ba9b10bab
richsueh <richardhsueh94@gmail.com>
2021-01-16 09:22:58 +0800

    add favicon

--- pages/_document.js
+++ pages/_document.js
@@ -36,7 +36,7 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
             rel="stylesheet"
           />
-
+          <link rel="icon" type="image/png" href="/icon.png" />
           <script
             async
             src="https://www.googletagmanager.com/gtag/js?id=UA-158052933-1"
