80772a4f6d7b3e29a26178488f8dff97f5385c70
richsueh <richardhsueh94@gmail.com>
2021-02-02 23:58:42 +0800

    add noto sans tc

--- pages/recipe/[slug].js
+++ pages/recipe/[slug].js
@@ -6,9 +6,9 @@ import { animated, useTransition, config } from "react-spring";
 import styled from "styled-components";
 import { format } from "date-fns";
 import queryString from "query-string";
-// import canvasTxt from "canvas-txt";
-// import { createCanvas, loadImage, registerFont } from "canvas";
-// import fs from "fs";
+import canvasTxt from "canvas-txt";
+import { createCanvas, loadImage, registerFont } from "canvas";
+import fs from "fs";
 
 import SEO from "../../components/seo";
 import Carousel from "../../components/carousel";
@@ -17,7 +17,7 @@ import RecipeQueryContext from "../../lib/RecipeQueryContext";
 import { processIngredient } from "../../lib/recipeHelper";
 import markdownToHtml from "../../lib/markdownToHtml";
 import useWindowSize from "../../lib/useWindowSize";
-// import { roundRect } from "../../lib/canvasHelper";
+import { roundRect } from "../../lib/canvasHelper";
 
 const RecipeContainer = styled(animated.div)`
   display: flex;
@@ -545,101 +545,109 @@ export async function getStaticProps({ params }) {
     "content",
   ]);
 
-
   const content = await markdownToHtml(post.content || "");
 
-  // const width = 540;
-  // const height = 281;
+  const width = 540;
+  const height = 281;
 
-  // registerFont("./public/fonts/Inter-Regular.ttf", {
-  //   family: "Inter",
-  //   weight: "regular",
-  // });
-  // registerFont("./public/fonts/Inter-Bold.ttf", {
-  //   family: "Inter",
-  //   weight: "bold",
-  // });
+  registerFont(path.resolve("./public/fonts/Inter-Regular.ttf"), {
+    family: "Inter",
+    weight: "regular",
+  });
+  registerFont(path.resolve("./public/fonts/Inter-Bold.ttf"), {
+    family: "Inter",
+    weight: "bold",
+  });
 
-  // const canvas = createCanvas(width, height);
-  // const context = canvas.getContext("2d");
+  registerFont(path.resolve("./public/fonts/NotoSansTC-Regular.ttf"), {
+    family: "NotoSansTC",
+    weight: "regular",
+  });
+  registerFont(path.resolve("./public/fonts/NotoSansTC-Bold.ttf"), {
+    family: "NotoSansTC",
+    weight: "bold",
+  });
 
-  // context.fillStyle = "#fff";
-  // context.fillRect(0, 0, width, height);
+  const canvas = createCanvas(width, height);
+  const context = canvas.getContext("2d");
 
-  // if (post.image && post.image[0]) {
-  //   const txt = post.recipe_name;
-  //   context.font = "Inter";
-  //   context.fillStyle = "#000";
-  //   canvasTxt.fontSize = 30;
-  //   canvasTxt.lineHeight = 35;
-  //   canvasTxt.fontWeight = "bold";
-  //   canvasTxt.align = "left";
-  //   canvasTxt.vAlign = "top";
-  //   canvasTxt.drawText(context, txt, 260, 40, 230, 90);
+  context.fillStyle = "#fff";
+  context.fillRect(0, 0, width, height);
 
-  //   canvasTxt.fontSize = 18;
-  //   canvasTxt.fontWeight = "regular";
-  //   canvasTxt.drawText(context, post.date, 260, 220, 230, 200);
+  if (post.image && post.image[0]) {
+    const txt = post.recipe_name;
+    context.font = "NotoSansTC";
+    context.fillStyle = "#000";
+    canvasTxt.fontSize = 30;
+    canvasTxt.lineHeight = 35;
+    canvasTxt.fontWeight = "bold";
+    canvasTxt.align = "left";
+    canvasTxt.vAlign = "top";
+    canvasTxt.drawText(context, txt, 260, 40, 230, 90);
 
-  //   canvasTxt.fontSize = 18;
-  //   canvasTxt.fontWeight = "bold";
-  //   canvasTxt.drawText(
-  //     context,
-  //     `${capitalizeFirstLetter(post.type)} recipe`,
-  //     260,
-  //     200,
-  //     230,
-  //     200
-  //   );
+    canvasTxt.fontSize = 18;
+    canvasTxt.fontWeight = "regular";
+    canvasTxt.drawText(context, post.date, 260, 220, 230, 200);
 
-  //   context.fillStyle = "transparent";
-  //   roundRect(context, 20, 20, 500, 240, 6, "#000", true);
+    canvasTxt.fontSize = 18;
+    canvasTxt.fontWeight = "bold";
+    canvasTxt.drawText(
+      context,
+      `${capitalizeFirstLetter(post.type)} recipe`,
+      260,
+      200,
+      230,
+      200
+    );
 
-  //   roundRect(context, 40, 40, 200, 200, 3, "#fff", true);
-  //   context.clip();
+    context.fillStyle = "transparent";
+    roundRect(context, 20, 20, 500, 240, 6, "#000", true);
 
-  //   loadImage(`./public/${post.image[0]}`).then((image) => {
-  //     context.drawImage(image, 40, 40, 200, 200);
-  //     const buffer = canvas.toBuffer("image/png");
+    roundRect(context, 40, 40, 200, 200, 3, "#fff", true);
+    context.clip();
 
-  //     fs.writeFileSync(
-  //       `./public/assets/recipe/preview-${post.slug}.png`,
-  //       buffer
-  //     );
-  //   });
-  // } else {
-  //   const txt = post.recipe_name;
-  //   context.font = "Inter";
-  //   context.fillStyle = "#000";
-  //   canvasTxt.fontSize = 30;
-  //   canvasTxt.lineHeight = 35;
-  //   canvasTxt.fontWeight = "bold";
-  //   canvasTxt.align = "left";
-  //   canvasTxt.vAlign = "top";
-  //   canvasTxt.drawText(context, txt, 40, 40, 250, 90);
+    loadImage(`./public/${post.image[0]}`).then((image) => {
+      context.drawImage(image, 40, 40, 200, 200);
+      const buffer = canvas.toBuffer("image/png");
 
-  //   canvasTxt.fontSize = 18;
-  //   canvasTxt.fontWeight = "regular";
-  //   canvasTxt.drawText(context, post.date, 40, 220, 230, 200);
+      fs.writeFileSync(
+        `./public/assets/recipe/preview-${post.slug}.png`,
+        buffer
+      );
+    });
+  } else {
+    const txt = post.recipe_name;
+    context.font = "NotoSansTC";
+    context.fillStyle = "#000";
+    canvasTxt.fontSize = 30;
+    canvasTxt.lineHeight = 35;
+    canvasTxt.fontWeight = "bold";
+    canvasTxt.align = "left";
+    canvasTxt.vAlign = "top";
+    canvasTxt.drawText(context, txt, 40, 40, 250, 90);
 
-  //   canvasTxt.fontSize = 18;
-  //   canvasTxt.fontWeight = "bold";
-  //   canvasTxt.drawText(
-  //     context,
-  //     `${capitalizeFirstLetter(post.type)} recipe`,
-  //     40,
-  //     200,
-  //     230,
-  //     200
-  //   );
+    canvasTxt.fontSize = 18;
+    canvasTxt.fontWeight = "regular";
+    canvasTxt.drawText(context, post.date, 40, 220, 230, 200);
 
-  //   context.fillStyle = "transparent";
-  //   roundRect(context, 20, 20, 500, 240, 6, "#000", true);
+    canvasTxt.fontSize = 18;
+    canvasTxt.fontWeight = "bold";
+    canvasTxt.drawText(
+      context,
+      `${capitalizeFirstLetter(post.type)} recipe`,
+      40,
+      200,
+      230,
+      200
+    );
 
-  //   const buffer = canvas.toBuffer("image/png");
+    context.fillStyle = "transparent";
+    roundRect(context, 20, 20, 500, 240, 6, "#000", true);
 
-  //   fs.writeFileSync(`./public/assets/recipe/preview-${post.slug}.png`, buffer);
-  // }
+    const buffer = canvas.toBuffer("image/png");
+
+    fs.writeFileSync(`./public/assets/recipe/preview-${post.slug}.png`, buffer);
+  }
 
   return {
     props: {
