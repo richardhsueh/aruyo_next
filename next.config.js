e1dec2e9f306931bba2e7bc94cab07df64fb1859
richsueh <richardhsueh94@gmail.com>
2021-02-20 22:09:53 +0800

    update package.json

--- next.config.js
+++ next.config.js
@@ -1,17 +1,9 @@
 const images = require("remark-images");
 const emoji = require("remark-emoji");
 const withPWA = require("next-pwa");
-const withMDX = require("@zeit/next-mdx")({
-  options: {
-    mdPlugins: [images, emoji],
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
   },
 });
-
-module.exports = withPWA(
-  withMDX({
-    pageExtensions: ["js", "jsx", "mdx"],
-    pwa: {
-      dest: "public",
-    },
-  })
-);
